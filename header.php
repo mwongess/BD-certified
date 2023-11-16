9bad077a92f7a262a1db6cf0b740adf984a4b650
mwongess <amosmwongelah@gmail.com>
2023-11-16 18:43:21 +0300

    update theme files

---
+++ header.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Contains the header
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('$show:string')?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BD Certified</title>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    
